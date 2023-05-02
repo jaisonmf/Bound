ab023c7c628f36c1ec678b5ddbc6ef3c8b934c62
jaisonmf <85539202+jaisonmf@users.noreply.github.com>
2023-05-02 11:20:09 +1000

    player health bar functionality

--- Assets/Scripts/Game Management/gameManager.cs
+++ Assets/Scripts/Game Management/gameManager.cs
@@ -12,7 +12,7 @@ public class gameManager : MonoBehaviour
     private void Start()
     {
         enemyGeneration();
-        //playerTurn();
+        playerTurn();
 
     }
 
--- Assets/Scripts/Game Management/playerManager.cs
+++ Assets/Scripts/Game Management/playerManager.cs
@@ -9,23 +9,28 @@ public class playerManager : MonoBehaviour
     [SerializeField] private enemyManager enemyManager;
 
     //playerStats
-    private int playerMaxHealth = 100;
+    [SerializeField] private int playerMaxHealth = 100;
     public int playerHealth;
     [SerializeField] private int playerDamage;
     private int playerMinDamage;
     private int playerMaxDamage;
 
+    //UI
     [SerializeField] private Button action1;
     [SerializeField] private Button action2;
     [SerializeField] private Button action3;
     [SerializeField] private Button action4;
+    [SerializeField] private Slider playerHealthbar;
 
+
+  
     private void Start()
     {
         action1.interactable = false;
         action2.interactable = false;
         action3.interactable = false;
         action4.interactable = false;
+
         playerHealth = playerMaxHealth;
     }
     public void PlayerTurn()
@@ -33,6 +38,14 @@ public class playerManager : MonoBehaviour
         if(playerHealth > playerMaxHealth)
         {
             playerHealth = playerMaxHealth;
+        }
+
+        if(playerHealth > 0)
+        {
+            action1.interactable = true;
+            action2.interactable = true;
+            action3.interactable = true;
+            action4.interactable = true;
         }
 
 
@@ -45,7 +58,8 @@ public class playerManager : MonoBehaviour
     {
         if(Button == 1)
         {
-
+            playerHealth -= 10;
+            UpdateHealthBar(playerHealth, playerMaxHealth);
         }
 
         if(Button == 2)
@@ -61,8 +75,18 @@ public class playerManager : MonoBehaviour
         if(Button == 4)
         {
             gameManager.enemyTurn();
+            action1.interactable = false;
+            action2.interactable = false;
+            action3.interactable = false;
+            action4.interactable = false;
         }
     }
 
+
+    public void UpdateHealthBar(float Currentvalue, float maxValue)
+    {
+        float percentageResult = Currentvalue / maxValue;
+        playerHealthbar.value = percentageResult;
+    }
 
 }
