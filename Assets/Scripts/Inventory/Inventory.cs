af9b9ff1f04aafb24269d2b78410f654a1b91779
jaisonmf <85539202+jaisonmf@users.noreply.github.com>
2023-05-16 12:01:13 +1000

    started doing inventory across scenes

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -1,8 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class Inventory : MonoBehaviour
 {
+    private playerInventory playerInventory;
+    public int inventoryCount;
+    [SerializeField] private GameObject parent;
+    private GameObject spawnedItem;
+    private float scaleMultiplier = 0.5f;
+
+    //Inventory Grid
+    private int columns = 3;
+    private float spacing = 1.0f;
+    private Vector2 gridSize = new Vector2(941f, 1163f);
+
+    private void Start()
+    {
+        playerInventory = GameObject.Find("playerStats").GetComponent<playerInventory>();
+
+        GenerateInventory();
+    }
+
+
+
+
+    public void GenerateInventory()
+    {
+       inventoryCount = playerInventory.inventory.Count;
+        int rows = Mathf.CeilToInt((float)inventoryCount / (float)columns);
+
+
+        float elementSizeX = gridSize.x / columns;
+        float elementSizeY = gridSize.y / columns;  
+
+
+
+        for(int i = 0; i < inventoryCount; i++)
+        {
+            int row = i / columns;
+            int column = i % columns;
+
+            Vector3 localPosition = new Vector3((column * elementSizeX) + (elementSizeX / 2f), (row * elementSizeY) + (elementSizeY / 2f), 0f);
+
+
+            Vector3 position = transform.TransformPoint(localPosition);
+
+            spawnedItem = Instantiate(playerInventory.Instance.inventory[i], position, Quaternion.identity, transform);
+            spawnedItem.transform.SetParent(parent.transform, false);
+            spawnedItem.transform.localScale = Vector3.one * scaleMultiplier;
+        }
+    }
 
 }
--- Assets/Scripts/Items/ItemScript.cs
+++ Assets/Scripts/Items/ItemScript.cs
@@ -1,10 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ItemScript : MonoBehaviour
 {
+    private playerInventory playerInventory;
+    private GameObject item;
+
+    public void Start()
+    {
+        playerInventory = GameObject.Find("playerStats").GetComponent<playerInventory>();
+        item = this.gameObject;
+    }
+
+    public void GrabItem()
+    {
+        if (playerInventory.inventory.Count != 9)
+        {
+            playerInventory.Instance.inventory.Add(item);
+        }
+        else
+        {
+            exit();
+        }
+    }
+
 
     public void exit()
     {
